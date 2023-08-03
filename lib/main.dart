398f43d57771f5f60da89ddb0e1b85cbbfda1e62
Rahul <rahulsharma3258@gmail.com>
2023-08-03 17:53:46 +0530

    Authentication StreamBuilder implemented

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ import 'package:get/get.dart';
 
 // Firebase
 import 'package:firebase_core/firebase_core.dart';
+import 'package:note_taking_app/Pages/home/home_page.dart';
+import 'package:note_taking_app/Pages/login/login_page.dart';
 import 'package:note_taking_app/firebase_options.dart';
+
+// Firebase Authentication
+import 'package:firebase_auth/firebase_auth.dart';
 
 // it will initialize our Flutter Firebase app before starting the App
 
@@ -32,7 +37,16 @@ class MyApp extends StatelessWidget {
       ),
       // use StreamBuilder for Firebase Authentication purpose (login/sign up Page Switching)
       // [ use Firebase Pre-Built userChanges() Method inside StreamBuilder ]
-      home: const Placeholder(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LogInPage();
+          }
+        },
+      ),
     );
   }
 }
