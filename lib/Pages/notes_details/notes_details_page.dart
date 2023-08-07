3a48eef5328987f7c4b3f2d0046c9a6df76381a1
Rahul <rahulsharma3258@gmail.com>
2023-08-07 12:17:09 +0530

    setting initial value for textfields and checking fields are empty or not while Updating the Note.

--- lib/Pages/notes_details/notes_details_page.dart
+++ lib/Pages/notes_details/notes_details_page.dart
@@ -40,6 +40,11 @@ class NotesDetailsPage extends StatelessWidget {
               Icons.edit,
             ),
             onTap: () {
+              // setting TextField's initial Text
+              topicController.text = topic;
+              descriptionController.text = description;
+
+              // showing bottomSheet
               notesDetailsController.isOpen.value = true;
             },
           ),
@@ -63,6 +68,9 @@ class NotesDetailsPage extends StatelessWidget {
         ),
       ),
       // Bottom Sheet for Updating the Note
+
+      // if isOpen variable's value is true then BottomSheet will become Visible
+      // and if isOpen variable's value is false then BottomSheet will become Invisible
       bottomSheet: Obx(
         () => Container(
           child: notesDetailsController.isOpen.value
@@ -70,6 +78,7 @@ class NotesDetailsPage extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
+                    // text field for entering topic
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
@@ -83,6 +92,7 @@ class NotesDetailsPage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    // text field for entering description
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextFormField(
@@ -98,27 +108,36 @@ class NotesDetailsPage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    // Update Button to Update the current Note with new Values
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10.0, vertical: 15.0),
                       child: ElevatedButton(
                         onPressed: () {
-                          // getting field data and storing into variables
-                          var topic = topicController.text;
-                          var description = topicController.text;
+                          // Checking fields
+                          if (topicController.text.isEmpty ||
+                              descriptionController.text.isEmpty) {
+                            Get.snackbar(
+                                'Empty Field !', 'Enter some data first');
+                          } else {
+                            // getting field data and storing into variables
+                            var topic = topicController.text;
+                            var description = topicController.text;
 
-                          // updating the note
-                          FirestoreServices.updateData(
-                              index, topic, description);
+                            // updating the note
+                            FirestoreServices.updateData(
+                                index, topic, description);
 
-                          // closing the Bottomsheet
-                          notesDetailsController.isOpen.value = false;
+                            // closing the Bottomsheet
+                            notesDetailsController.isOpen.value = false;
 
-                          Get.snackbar('Note Updated !', '');
+                            Get.snackbar('Note Updated !', '');
+                          }
                         },
                         child: const Text('Update'),
                       ),
                     ),
+                    // Close buttom to close the BottomSheet
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10.0, vertical: 15.0),
